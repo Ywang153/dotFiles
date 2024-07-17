924aa8e76409d7278de87624ae48adf6a319dd9b
Glenn WANG <wyhbetter@outlook.com>
2024-07-17 16:43:50 +0800

    update port-instact.sh

--- .env/usr/bin/port-instact.sh
+++ .env/usr/bin/port-instact.sh
@@ -6,12 +6,23 @@ target="$1"
 while [ 1 ]
 do
     echo trying to install "$target"
-    if [[ $(port installed "$target" | grep "$target") ]]; then echo "$target" present; break; fi
-    line=`port install "$target" 2>&1 > /dev/tty | grep "port -f activate"`
+    
+    if [[ $(port installed "$target" | grep "$target") ]];
+    then
+        echo "$target" present; break;
+    fi
+    
+    line=`yes | port install "$target" 2>&1 > /dev/tty | grep "port -f activate"`
     package=$(echo $line | sed -e "s/^.*port -f activate //" -e "s/. .*//")
-    if [ "$package" = "$oldpackage" ]; then echo double \""$package"\"; break; fi
-    oldpackage="$package"
+    
+    if [ "$package" = "$oldpackage" ];
+    then
+        echo double \""$package"\"; break;
+    fi
+    
     echo activating "$package"
+    
+    oldpackage="$package"
     port -f activate "$package"
 done
 
